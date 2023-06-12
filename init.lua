0294ef9d5d79846e95090076496e9aff66bc637b
hou5emd <andrandrey92@gmail.com>
2023-06-12 19:44:36 +0500

    Init

---
+++ init.lua
@@ -0,0 +1,12 @@
+-- Basic
+require('core.plugins')
+require('core.mappings')
+require('core.configs')
+
+-- Plugins
+require('plugins.neotree')
+require('plugins.treesitter')
+require('plugins.lsp')
+
+-- AutoStarts
+require('plugins.theme')
---
+++ lua/core/mappings.lua
@@ -0,0 +1,6 @@
+vim.g.mapleader = " "
+
+-- Neo-Tree MAP
+
+vim.keymap.set('n', '<leader>e', ':Neotree left toggle<CR>')
+vim.keymap.set('n', '<leader>o', ':Neotree left git_status<CR>')
